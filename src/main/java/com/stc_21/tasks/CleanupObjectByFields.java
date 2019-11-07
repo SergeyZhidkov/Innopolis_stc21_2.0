f51f798db5abfed49e0f190c03da1e2252c0ea09
Sergey Zhidkov <zhidkovsa81@mail.ru>
2019-11-07 12:20:38 +0300

    Add class CleanupObjectByFields

---
+++ src/main/java/com/stc_21/tasks/CleanupObjectByFields.java
@@ -0,0 +1,10 @@
+package com.stc_21.tasks;
+
+import java.util.Set;
+
+public class CleanupObjectByFields {
+
+    public void cleanup(Object object, Set<String> fieldsToCleanup, Set<String> fieldsToOutput){
+        
+    }
+}
